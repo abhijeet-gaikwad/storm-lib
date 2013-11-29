77daad632e683d378f3d371f4ee3066410423cac
Abhijeet <abhijeet@abhijeet-pc.(none)>
2013-11-28 21:24:14 -0600

    project.clj fixed.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject storm-lib "1.0.0-SNAPSHOT"
   :description "storm-lib"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [storm/storm "0.9.0-rc2"]]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :target-path "target"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [storm/storm "0.9.0-rc3"]]
   :aot :all)
